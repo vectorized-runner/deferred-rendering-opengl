fcc802ccc9cedb12251ea40037b6f666395393f9
Bartu Baş <bartubas99@gmail.com>
2023-06-27 15:18:05 +0300

    Add frag def geometry

--- SetupOpenGLExample/shaders/frag_deferred_geometry.glsl
+++ SetupOpenGLExample/shaders/frag_deferred_geometry.glsl
@@ -1,10 +1,23 @@
 #version 410 core
+layout (location = 0) out vec3 gPosition;
+layout (location = 1) out vec3 gNormal;
+layout (location = 2) out vec4 gAlbedoSpec;
 
-uniform vec3 unlit;
+in vec2 TexCoords;
+in vec3 FragPos;
+in vec3 Normal;
 
-out vec4 fragColor;
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
 
-void main(void)
+void main()
 {
-    fragColor = vec4(unlit, 1);
+    // store the fragment position vector in the first gbuffer texture
+    gPosition = FragPos;
+    // also store the per-fragment normals into the gbuffer
+    gNormal = normalize(Normal);
+    // and the diffuse per-fragment color
+    gAlbedoSpec.rgb = texture(texture_diffuse1, TexCoords).rgb;
+    // store specular intensity in gAlbedoSpec's alpha component
+    gAlbedoSpec.a = texture(texture_specular1, TexCoords).r;
 }
