525f6d9cce593e8ecd2daaf8e2e97b04a3f8c5b2
Bartu Baş <bartubas99@gmail.com>
2023-06-27 17:02:03 +0300

    Remove light pos completely

--- SetupOpenGLExample/shaders/frag_deferred_light.glsl
+++ SetupOpenGLExample/shaders/frag_deferred_light.glsl
@@ -7,17 +7,12 @@ uniform sampler2D gPosition;
 uniform sampler2D gNormal;
 uniform sampler2D gAlbedoSpec;
 
-struct Light {
-    vec3 Position;
-    vec3 Color;
-    
-    float Linear;
-    float Quadratic;
-    float Radius;
-};
-const int NR_LIGHTS = 32;
-uniform Light lights[NR_LIGHTS];
-uniform vec3 viewPos;
+const int maxLightCount = 100;
+
+uniform vec3 lightPositions[maxLightCount];
+uniform vec3 lightIntensities[maxLightCount];
+uniform vec3 cameraPos;
+uniform int lightCount;
 
 void main()
 {
@@ -29,15 +24,16 @@ void main()
     
     // then calculate lighting as usual
     vec3 lighting  = Diffuse * 0.1; // hard-coded ambient component
-    vec3 viewDir  = normalize(viewPos - FragPos);
-    for(int i = 0; i < NR_LIGHTS; ++i)
+    vec3 viewDir  = normalize(cameraPos - FragPos);
+    for(int i = 0; i < lightCount; ++i)
     {
         // calculate distance between light source and current fragment
         float distance = length(lights[i].Position - FragPos);
         if(distance < lights[i].Radius)
         {
             // diffuse
-            vec3 lightDir = normalize(lights[i].Position - FragPos);
+            vec3 lightPos = lightPositions[i];
+            vec3 lightDir = normalize(lightPos - FragPos);
             vec3 diffuse = max(dot(Normal, lightDir), 0.0) * Diffuse * lights[i].Color;
             // specular
             vec3 halfwayDir = normalize(lightDir + viewDir);
