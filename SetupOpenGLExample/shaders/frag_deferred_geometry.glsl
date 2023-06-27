7775b58b145d6292bae0c86e32d870d2cf2fdd70
Bartu Baş <bartubas99@gmail.com>
2023-06-27 18:17:06 +0300

    Disable texcoords usage in frag_deferred

--- SetupOpenGLExample/shaders/frag_deferred_geometry.glsl
+++ SetupOpenGLExample/shaders/frag_deferred_geometry.glsl
@@ -3,12 +3,15 @@ layout (location = 0) out vec3 gPosition;
 layout (location = 1) out vec3 gNormal;
 layout (location = 2) out vec4 gAlbedoSpec;
 
-in vec2 TexCoords;
+// in vec2 TexCoords;
 in vec3 FragPos;
 in vec3 Normal;
 
-uniform sampler2D texture_diffuse1;
-uniform sampler2D texture_specular1;
+// uniform sampler2D texture_diffuse1;
+// uniform sampler2D texture_specular1;
+
+vec3 kd = vec3(1, 0.2, 0.2);     // diffuse reflectance coefficient
+vec3 ks = vec3(0.8, 0.8, 0.8);   // specular reflectance coefficient
 
 void main()
 {
@@ -16,8 +19,13 @@ void main()
     gPosition = FragPos;
     // also store the per-fragment normals into the gbuffer
     gNormal = normalize(Normal);
+    
     // and the diffuse per-fragment color
-    gAlbedoSpec.rgb = texture(texture_diffuse1, TexCoords).rgb;
+    // gAlbedoSpec.rgb = texture(texture_diffuse1, TexCoords).rgb;
+    gAlbedoSpec.rgb = kd;
+
     // store specular intensity in gAlbedoSpec's alpha component
-    gAlbedoSpec.a = texture(texture_specular1, TexCoords).r;
+    // gAlbedoSpec.a = texture(texture_specular1, TexCoords).r;
+    gAlbedoSpec.a = ks.x;
+
 }
