4e52cfaef2fdaba77f74719db6c9ba4645fba1a0
Bartu Baş <bartubas99@gmail.com>
2023-06-27 17:30:40 +0300

    Remove clear part -- we already do it

--- SetupOpenGLExample/main.cpp
+++ SetupOpenGLExample/main.cpp
@@ -1379,10 +1379,6 @@ void DrawSceneForward(){
 void DrawSceneDeferred(){
     
     /*
-    // render
-    // ------
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // 1. geometry pass: render scene's geometry/color data into gbuffer
     // -----------------------------------------------------------------
